1ac796e3ee7329a7fb921326aa1c49b2a522d921
hyojk2001 <32731442+hyojk2001@users.noreply.github.com>
2021-03-04 17:52:14 +0900

    Add files via upload

--- lecture04.js
+++ lecture04.js
@@ -1,5 +1,10 @@
 // var name = prompt("이름을 입력해 주세요.");
 // console.log(name,"님 환영합니다.")
 
-var msg = "출력될 문자열입니다.";
-console.log(msg)
+// var msg = "출력될 문자열입니다.";
+// console.log(msg)
+
+// var msg;
+// msg = "Hello world"
+// // alert(msg)
+// console.log(msg)
---
+++ lecture05.js
@@ -0,0 +1,14 @@
+var a = 3.14
+var c = '안녕하세요'
+var e = false
+
+console.log(a,typeof(a))
+console.log(c,typeof(c))
+console.log(e,typeof(e))
+
+var n = 1
+console.log(n,typeof(n))
+var b = true
+console.log(b,typeof(b))
+var s = '문자열입니다'
+console.log(s,typeof(s))
