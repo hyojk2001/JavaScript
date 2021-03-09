6ae57c122ffc584d04614b16d04257783c83a1e3
hyojk2001 <32731442+hyojk2001@users.noreply.github.com>
2021-03-09 17:39:55 +0900

    Create lecture12.js

---
+++ lecture12.js
@@ -0,0 +1,10 @@
+var str = 'abcdeabcde'
+str.charAt(0) # 첫 인덱스는 0으로 시작
+str.charAt(-1) # 마지막 글자
+str.length(str) # 문자열 길이 출력
+
+str.substring(2,4)  # 2번째 이상 4번째 미만 글자 출력
+str.substr(2,4)     # 시작 인덱스 2에서 시작해서 4글자 출력
+str.substr(-7,2)    # 뒤에서부터 7번재 자리에서 시작해서 2글자 출력
+str.indexof()       #인덱스 출력
+str.lastIndexOf     #마지막 인덱스 출력
